f5b771c7565913f0050300177f0803e990ce4508
kazim akgul <kazimakgul@gmail.com>
2012-02-20 21:46:41 +0200

    first configuration including DB

--- app/Config/core.php
+++ app/Config/core.php
@@ -184,12 +184,12 @@
 /**
  * A random string used in security hashing methods.
  */
-	Configure::write('Security.salt', 'DYhG93b0qyJfIxfs2guVoUubWwvniR2G0FgaC9mi');
+	Configure::write('Security.salt', 'VoUubWwvniR2G0FgaC9miDYhG93b0qyJfIxfs2gu');
 
 /**
  * A random numeric string (digits only) used to encrypt/decrypt strings.
  */
-	Configure::write('Security.cipherSeed', '76859309657453542496749683645');
+	Configure::write('Security.cipherSeed', '74249674968366859309657453545');
 
 /**
  * Apply timestamps with the last modified time to static assets (js, css, images).
